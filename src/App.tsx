94852fc07fdf23a180b8beac9feba7922028b493
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-04-25 20:07:17 +0000

    Update babel + eslint

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Maraba helevole</h1>
+      <p>Haru Haru</p>
+      <strong>Baru Baru</strong>
     </div>
   );
 }
